b316cc03c794e8f2acc9a1a189414b3deb750728
stan-2021131 <sergiocoromac@gmail.com>
2025-01-29 14:27:38 -0600

    Creación de clase Main con un pequeño ejemplo del uso de la pila

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        char[] operators = {'+', '-', '*', '/', '%'};
+        Stack myStack = new Stack<Integer>();
+        /* Ejemplo base del funcionamiento de la pila 
+        myStack.push(1);
+        myStack.push(2);
+        int b = myStack.pop();
+        int a = myStack.pop();
+        //El operador se pasa como un texto, entre ''
+        System.out.println("El resultado de la operación es: " + myStack.operation('+', a, b));
+        //Condicional que compara el último y primer elemento de la lista
+        if(myStack.getLast() == myStack.getFirst() ){
+            System.out.println("Son el mismo objeto. Final del stack");
+        }else{
+            System.out.println("No, pos entonces no son lo mismo. Quedan objetos en el stack :p");
+        }
+        */
+    }
+}
