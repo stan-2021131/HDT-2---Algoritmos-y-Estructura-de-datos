f63c017641a54775809a0d965fc4febdb9ff7157
unknown <alejandraviles2005@gmail.com>
2025-01-30 17:14:36 -0600

    Se analizó la logística del programa al devolver resultados y se mejoró estéticamente ese aspecto. También se trabajó el manejo de errores al calcular una operación con caracteres inválidos sin que afecte el flujo del programa para continuar con las siguientes operaciones.

--- ADTCalculator.java
+++ ADTCalculator.java
@@ -4,44 +4,54 @@ import java.io.IOException;
 
 public class ADTCalculator {
     char[] operators = {'+', '-', '*', '/', '%'};
+
     @SuppressWarnings("unchecked")
     public void calculate (String file){
         @SuppressWarnings("rawtypes")
         Stack myStack = new Stack<Integer>();
+
         //Nombre del archivo donde estarán las operaciones. Cambiarlo si se quiere usar otro archivo
         try(BufferedReader bufferedReader = new BufferedReader(new FileReader(file))){
             String line;
             while ((line = bufferedReader.readLine()) != null){
                 clear(myStack);
                 String[] data = line.split(" ");
-                for(String value: data){
+                boolean error = false;
+
+                for(String value : data){
                     //Revisa si el valor es positivo
                     if(value.matches("\\d+")){
                         myStack.push(Integer.valueOf(value));
-                    }else if(value.length() == 1 && isOperator(value.charAt(0))){
+                    }else if(value.length() == 1 && isOperator(value.charAt(0))){ //Es operador
                         //Verifica si hay suficientes operandos en la pila
                         if(size(myStack) < 2){
-                            System.out.println("There are not enough operands");
+                            System.out.println("Error: There are not enough operands in line: " + line);
+                            error = true;
                             clear(myStack);
-                            return;
+                            break;
+                            //return;
                         }
                         //Obtiene los últimos 2 valores agregados a la pila y el operador a usar
                         int b = ((Integer) myStack.pop());
                         int a = ((Integer) myStack.pop());
                         char operator = value.charAt(0);
+
                         int result = (Integer) myStack.operation(operator, a, b);
                         myStack.push(result);
                     }else{
                         //Si el dato no puede ser operado limpia la pila y no regresa un resultado
-                        System.out.println("Invalid value" + value);
+                        System.out.println("Error: Invalid value '" + value + "' in line: " + line);
+                        error = true;
                         clear(myStack);
-                        return;
+                        break;
                     }
                 }
-                if (size(myStack) == 1) {
-                    System.out.println("The result of the line is: " + myStack.pop());
-                } else {
-                    System.out.println("Error: There is no single final result on the stack for this line.");
+                if (!error){    //La operación puede ser efectuada sin problemas
+                    if (size(myStack) == 1) {
+                        System.out.println("The result of the line (" + line + ") is: " + myStack.pop());
+                    } else { // Hay más de un elemento en la stack y no hay más operadores
+                        System.out.println("Error: There is no single final result on the stack for the line: " + line);
+                    }
                 }
             }
         } catch (IOException e) {
@@ -49,6 +59,7 @@ public class ADTCalculator {
         } 
     }
 
+    // Validando los operadores ingresados
     public boolean isOperator(char operator){
         for(char validOpeator : operators)
             if(operator == validOpeator) return true;
